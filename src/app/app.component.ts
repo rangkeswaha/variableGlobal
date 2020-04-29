e11e32c07d646cc89cc586beea62d31a3da96b2f
rangkeswaha <c14180243@john.petra.ac.id>
2020-04-29 10:05:25 +0700

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobSerService } from './glob-ser.service';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  datadariglob = '';
+  constructor (public variabelglobal : GlobSerService){
+    this.datadariglob = this.variabelglobal.getData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GlobSerService } from './glob-ser.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [GlobSerService]
 })
 export class AppModule { }
